1469bafdf38a6976e87cf4440879f02209fae1f9
Carl Helgegren <carl.helgegren@live.se>
2022-04-03 11:51:27 +0200

    updated shape interface

--- w2/assignments/d1/a1_w1d2-d3_shape_interface.cpp
+++ w2/assignments/d1/a1_w1d2-d3_shape_interface.cpp
@@ -15,57 +15,62 @@ considered. Only, and if only both area and perimeter are equal then the objects
 using namespace std;
 
 class Shape {
-    // ATTRIBUTES6
     public:
-        int width{}, height{}, radius{}, area{};
-    // METHODS
-    protected:
+        // METHODS
+        void PrintArea() { cout << this->CalcArea() << endl; }
+        void PrintPerimeter() { cout << this->CalcPerimeter() << endl; }
         void SetWidth(int _width) { this->width = _width; };
         void SetHeight(int _height) { this->height = _height; };
         void SetRadius(int _radius) { this->radius = _radius; };
-        Shape() = default;
-        Shape(int _radius) { this->radius = _radius; };
+    protected:
+        // ATTRIBUTES6
+        int width{}, height{}, radius{}, area{}, perimeter{};
+        // METHODS
+        Shape(int _radius) { this->radius = _radius; cout; };
         Shape(int _width, int _height) { this->width = _width; this->height = _height; };
-        ~Shape() { cout << "Im Dead!" << endl; };
-        virtual void CalcArea() = 0;
+        virtual int CalcArea() = 0;
+        virtual int CalcPerimeter() = 0;
 };
 
 class Rectangle : public Shape {
-    // METHODS
+    private:
+        // METHODS
+        int CalcArea() { this->area = this->width * this->height; return this->area; };
+        int CalcPerimeter() { this->perimeter = this->width * 2 + this->height * 2; return this->perimeter; };
     public:
-        Rectangle() = default;
-        Rectangle(int _width, int _height) { this->width = _width; this->height = _height; cout << "A Rectangle was born" << endl; };
-        void CalcArea() { this->area = this->width * this->height; };
+        // METHODS
+        Rectangle(int _width, int _height) : Shape(_width, _height) {};
 };
 
-class Square : public Rectangle {
-    // METHODS
-    public:
-        Square() = default;
-        Square(int _width, int _height) { this->width = _width; this->height = _height; cout << "A Square was born" << endl; };
-        void CalcArea() { this->area = this->width * this->height; };
-};
+// class Square : public Rectangle {
+//     // METHODS
+//     public:
+//         Square() = default;
+//         Square(int _width, int _height) { this->width = _width; this->height = _height; cout << "A Square was born" << endl; };
+//         void CalcArea() { this->area = this->width * this->height; };
+// };
 
-class Circle : public Shape {
-    // METHODS
-    public:
-        Circle() = default;
-        Circle(int _radius) { this->radius = _radius; cout << "A Circle was born" << endl; };
-        void CalcArea() { this->area = PI * radius * radius; };
-};
+// class Circle : public Shape {
+//     // METHODS
+//     public:
+//         Circle() = default;
+//         Circle(int _radius) { this->radius = _radius; cout << "A Circle was born" << endl; };
+//         void CalcArea() { this->area = PI * radius * radius; };
+// };
 
 int main () {
     Rectangle r(10,10);
-    Circle c(5);
-    Square s(5,5);
-    cout << r.area << endl;
-    r.CalcArea();
-    cout << r.area << endl;
-    cout << c.area << endl;
-    c.CalcArea();
-    cout << c.area << endl;
-    cout << s.area << endl;
-    s.CalcArea();
-    cout << s.area << endl;
+    // Circle c(5);
+    // Square s(5,5);
+    r.PrintArea();
+    r.PrintPerimeter();
+    r.SetHeight(20);
+
+    // cout << c.area << endl;
+    // c.CalcArea();
+    // cout << c.area << endl;
+    // cout << s.area << endl;
+    // s.CalcArea();
+    // cout << s.area << endl;
     return 0;
 }
